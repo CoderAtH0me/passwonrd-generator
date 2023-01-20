bd9d20c010dcbf3a16d93b6eab612ca53cfb6b08
CoderAtH0me <CoderAtH0me@users.noreply.github.com>
2023-01-20 20:45:37 +0100

    toggle-switch

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
 "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
 "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+
+
 function whatCharacters () {
-  let checkBox = document.getElementById("characterCheck");
+  let checkBox = document.getElementById("myCheck");
   if (checkBox.checked) {
     characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
 "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
