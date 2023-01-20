c86d1e449f3f6ffbd24fe62fcb64f4400fb86668
CoderAtH0me <CoderAtH0me@users.noreply.github.com>
2023-01-20 15:26:37 +0100

    0 functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
+"d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
+"9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+let outputOne = document.querySelector("#output-one")
+let outputTwo = document.querySelector("#output-two")
+function getPass (output) {
+    let password = ""
+    for (i = 1; i <= 15; i++) {
+        let randomIndex = Math.floor(Math.random()*characters.length);
+        password += characters[randomIndex];
+    }
+    output.textContent = password;
+}
+
+
+
