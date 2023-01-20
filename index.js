d6d4df24815b5c157e07ad732dd1021d54fe7749
CoderAtH0me <CoderAtH0me@users.noreply.github.com>
2023-01-20 19:28:19 +0100

    characters choose added

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
+
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
 "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
 "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+function whatCharacters () {
+  let checkBox = document.getElementById("characterCheck");
+  if (checkBox.checked) {
+    characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
+"d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
+"9"];
+  } else {
+    characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
+  "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", 
+  "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+};
+}
+
 let outputOne = document.querySelector("#output-one")
 let outputTwo = document.querySelector("#output-two")
 function getPass (output) {
@@ -11,6 +25,4 @@ function getPass (output) {
     }
     output.textContent = password;
 }
-
-
 
